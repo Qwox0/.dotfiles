d1bde2ed8daa2ea23a86cc6a6414cd767c576dc8
Qwox <qwox@qwox.com>
2023-01-17 02:16:16 +0100

    feat: improved install.sh script

--- install.sh
+++ install.sh
@@ -1,62 +1,70 @@
 #!/bin/bash
 # args must be config folders like `nvim`
 
+########## Config
 do_relinks=false
 
 script_path="$(dirname $(readlink -f $0))"
 
-print_err() {
-    printf "ERR: $@\n"
+########## Functions
+print() {
+    ident=""
+    for (( c=0; c<$1; ++c)); do
+        ident+=" "
+    done
+    #echo "$ident${@:2}"
+    printf "$ident${@:2}"
 }
+
 ll_dir() {
     # -d: show directory instead of content
+    print 4 "ls: "
     ls -aldhF --color=auto $@
 }
 
+########## Main
 if [[ $# == 0 ]]; then # install all dotfiles
     directories=$(find "$script_path" -maxdepth 1 -type d -not -name "\.*")
-else
+else # install given dirs
     directories=$(find "$@" -maxdepth 0 -type d -not -name "\.*" -exec readlink -f {} \;)
 fi
 
-if [[ $directories == "" ]]; then exit 1; fi
+for config_path in $directories; do
+    name=$(basename "$config_path")
+    name=${name^^}
 
-for config in $directories; do
-    printf "\n"
-    printf "found config: $config\n"
+    print 0 "\n$name:\n"
+    print 4 "Found config: $config_path\n"
 
-    target_file="$config/TARGET"
-    if [[ ! -f $target_file ]]; then print_err "$target_file is missing!"; continue; fi
+    target_file="$config_path/TARGET"
+    if [[ ! -f $target_file ]]; then print 4 "ERR: $target_file is missing!\n"; continue; fi
 
-    target=""
-    source $target_file     # read $target
-    if [[ $target == "" ]]; then print_err "cannot read \$target from $target_file"; continue; fi
-    printf "found target: $target\n"
-
+    target=$(source $target_file)
+    if [[ $target == "" ]]; then print 4 "ERR: cannot read \$target from $target_file\n"; continue; fi
+    print 4 "Found target: $target\n"
 
     if [[ -L "$target" ]]; then
-        printf "$target already exists and is a symbolic link\n"
+        print 0 "\n"
+        print 4 "\"$target\" already exists and is a symbolic link.\n"
         ll_dir "$target"
-        if $do_relinks; then
-            printf "deleting $target\n"
+        old_link_target=$(readlink -f "$target")
+        if [[ $old_link_target != $config_path ]]; then
+            print 4 "Incorrect link target!\n"
+            print 4 "Deleting link: \`rm $target\`\n"
             rm "$target"
         fi
-    fi
-
-    if [[ -d "$target" && ! -L "$target" ]]; then
-        printf "$target already exists and is a directory\n"
+    elif [[ -d "$target" ]]; then
+        print 0 "\n"
+        print 4 "$target already exists and is a directory.\n"
         ll_dir "$target"
-        printf "Trying \`rmdir $target\`\n"
+        print 4 "Deleting directory if empty: \`rmdir $target\`\n"
         rmdir "$target"
     fi
 
     if [[ -e "$target" ]]; then
-        printf "skipping link creation\n"
+        print 5 "=> Skipping link creation\n\n"
     else
-        printf "link $target -> $config\n"
-        ln -s "$config" "$target"
+        print 5 "=> Linking \"$target\" -> \"$config_path\"\n\n"
+        ln -s "$config_path" "$target"
     fi
 done
-
-printf "\n"
-
