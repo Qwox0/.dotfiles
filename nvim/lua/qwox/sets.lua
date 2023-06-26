5b68b0739ae6d271feb5c3d61f2d13951935e564
Qwox <qwox@qwox.com>
2023-06-26 12:12:23 +0200

    nvim: format sets.lua

--- nvim/lua/qwox/sets.lua
+++ nvim/lua/qwox/sets.lua
@@ -1,49 +1,50 @@
 -- vim.g    = let (in vimscript)
 -- vim.opt  = set (in vimscript)
 
-vim.opt.hidden = true       -- hide vim in bg when closing
+vim.opt.hidden = true         -- hide vim in bg when closing
 
-vim.opt.number = true           -- show line numbers
-vim.opt.relativenumber = true   -- show relative numbers
-vim.opt.signcolumn = "yes"      -- have column for symbols
+vim.opt.number = true         -- show line numbers
+vim.opt.relativenumber = true -- show relative numbers
+vim.opt.signcolumn = "yes"    -- have column for symbols
 vim.opt.colorcolumn = "100"
 
-vim.opt.tabstop = 4         -- tab: tab char length
-vim.opt.shiftwidth = 4      -- tab: indentation length
-vim.opt.expandtab = true    -- tab: convert tabs to spaces
-vim.opt.softtabstop = 4     -- tab: convert tabs to spaces
-vim.opt.smarttab = true     -- tab:
-vim.opt.autoindent = true   -- tab:
-vim.opt.smartindent = true  -- tab:
+vim.opt.tabstop = 4          -- tab: tab char length
+vim.opt.shiftwidth = 4       -- tab: indentation length
+vim.opt.expandtab = true     -- tab: convert tabs to spaces
+vim.opt.softtabstop = 4      -- tab: convert tabs to spaces
+vim.opt.smarttab = true      -- tab:
+vim.opt.autoindent = true    -- tab:
+vim.opt.smartindent = true   -- tab:
 
-vim.opt.scrolloff = 8       -- scroll before cursor reaches edge
+vim.opt.scrolloff = 8        -- scroll before cursor reaches edge
 
-vim.opt.termguicolors = true    -- colors: all 24-bit RGB colors
+vim.opt.termguicolors = true -- colors: all 24-bit RGB colors
 
-vim.opt.errorbells = false      -- audio: no bells
+vim.opt.errorbells = false   -- audio: no bells
 
 -- for listchars: see after/plugin/indent.lua
 
-vim.opt.swapfile = false    -- swap: buffer in memory not swap
-vim.opt.updatetime = 50     --
-vim.opt.backup = false      -- backup: no backup of edited file
-vim.opt.undofile = true     -- backup: store undo buffer in file
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- backup: store undofiles in '~/.vim/undodir'
+vim.opt.swapfile = false                  -- swap: buffer in memory not swap
+vim.opt.updatetime = 50                   --
+vim.opt.backup = false                    -- backup: no backup of edited file
+vim.opt.undofile = true                   -- backup: store undo buffer in file
+local home = require("qwox.util").paths.home
+vim.opt.undodir = home .. "/.vim/undodir" -- backup: store undofiles in '~/.vim/undodir'
 
-vim.opt.clipboard = "unnamedplus" -- clipboard: shared with OS (otherwise use '"*y' and '"*p')
+vim.opt.clipboard = "unnamedplus"         -- clipboard: shared with OS (otherwise use '"*y' and '"*p')
 
-vim.opt.hlsearch = false    -- search: doesn't stay highlighted
-vim.opt.incsearch = true    -- search:
+vim.opt.hlsearch = false                  -- search: doesn't stay highlighted
+vim.opt.incsearch = true                  -- search:
 
-vim.opt.ignorecase = true   -- search/cmds: ignore case
-vim.opt.smartcase = true    -- search/cmds: uppercase -> ignorecase: OFF
+vim.opt.ignorecase = true                 -- search/cmds: ignore case
+vim.opt.smartcase = true                  -- search/cmds: uppercase -> ignorecase: OFF
 
-vim.opt.cmdheight = 2       -- messages: more space
+vim.opt.cmdheight = 2                     -- messages: more space
 
 
-vim.opt.lazyredraw = true   -- performance: dont redraw on macros etc.
+vim.opt.lazyredraw = true -- performance: dont redraw on macros etc.
 
-vim.opt.splitright = true   -- split: to right (instead of left) on ':vsplit'
+vim.opt.splitright = true -- split: to right (instead of left) on ':vsplit'
 vim.opt.splitbelow = false
 
 --vim.opt.shortmess:append "c"      -- Don't show the dumb matching stuff.
