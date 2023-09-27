115c282f9608fafd98670177c3b6b4e9f19d2964
Qwox <qwox@qwox.com>
2023-09-27 02:18:29 +0200

    nvim: templates

---
+++ nvim/lua/qwox/templates.lua
@@ -0,0 +1,11 @@
+local f = io.popen("find " .. require("qwox.util").paths.nvim_config .. "/templates -mindepth 1")
+if f == nil then return end
+
+local autocmd = require("qwox.autocmd")
+
+---@param path string
+for path in f:lines() do
+    ---@type string
+    local ext = path:sub0((path:rfind0("%.") or -1) + 1)
+    autocmd("BufNewFile", { pattern = "*." .. ext, command = ":0r " .. path })
+end
