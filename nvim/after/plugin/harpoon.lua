a36e6afbe2be888f296bfd1a5a99583ef9d317c2
Qwox <qwox@qwox.com>
2023-10-06 13:23:53 +0200

    add harpoon map desc

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -5,10 +5,10 @@ local ui = require("harpoon.ui")
 
 local nmap = require("qwox.keymap").nmap
 
-nmap("<leader>a", function() mark.add_file() end)
-nmap("<C-e>", function() ui.toggle_quick_menu() end)
+nmap("<leader>a", function() mark.add_file() end, { desc = "[A]dd Harpoon mark" })
+nmap("<C-e>", function() ui.toggle_quick_menu() end, { desc = "Open Harpoon [E]dit menu" })
 
-nmap("<C-j>", function() ui.nav_file(1) end)
-nmap("<C-k>", function() ui.nav_file(2) end)
-nmap("<C-l>", function() ui.nav_file(3) end)
-nmap("<C-;>", function() ui.nav_file(4) end)
+nmap("<C-j>", function() ui.nav_file(1) end, { desc = "to Harpoon 1" })
+nmap("<C-k>", function() ui.nav_file(2) end, { desc = "to Harpoon 2" })
+nmap("<C-l>", function() ui.nav_file(3) end, { desc = "to Harpoon 3" })
+nmap("<C-;>", function() ui.nav_file(4) end, { desc = "to Harpoon 4" })
