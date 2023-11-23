9e6a232cb5244f38227b74c43e231a4821769f09
Qwox <qwox@qwox.com>
2023-11-23 18:07:20 +0100

    tampermonkey: add CookiePopupRemover

---
+++ tampermonkey/cookie_popup_remover.user.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         CookiePopupRemover
+// @namespace    qwox
+// @version      0.1
+// @description  removes cookie popups
+// @author       Qwox
+// @icon
+// @match        https://askubuntu.com/questions/*
+// @grant        none
+// @updateURL    https://github.com/Qwox0/.dotfiles/raw/main/tampermonkey/cookie_popup_remover.user.js
+// @downloadURL  https://github.com/Qwox0/.dotfiles/raw/main/tampermonkey/cookie_popup_remover.user.js
+// @sandbox      JavaScript
+// ==/UserScript==
+
+const PAGES = {
+    "askubuntu.com": () => document.querySelector("body > div.js-consent-banner"),
+}
+
+function getSite() {
+    return new window.URL(window.location.href);
+}
+
+function main() {
+    const site = getSite();
+    console.log("site:", site);
+    if (!site) throw "Error with getSite!";
+    const hostname = site.hostname;
+
+    const elemGetter = PAGES[hostname];
+    console.log("elemGetter:", elemGetter);
+    if (!elemGetter) throw `Please add a getter for "${hostname}"`;
+
+    const element = elemGetter();
+    console.log("element:", element);
+    if (!element) throw `Cannot get element.`;
+
+    element.style.display = "none";
+}
+
+let hasExecuted = false;
+
+function tmMain() {
+    if (hasExecuted) return;
+    hasExecuted = true;
+    console.log("executing Tampermonkey script...");
+    main();
+}
+
+(function() {
+    'use strict';
+    tmMain();
+    window.addEventListener('DOMContentLoaded', tmMain);
+})();
