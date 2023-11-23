e15851a9a149d616597e6a72d7b978cc0ff28611
Qwox <qwox@qwox.com>
2023-11-23 17:49:44 +0100

    tampermonkey template add url

--- tampermonkey/template.user.js
+++ tampermonkey/template.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         template
 // @namespace    qwox
-// @version      0.1
+// @version      0.1.1
 // @description  description
 // @author       Qwox
 // @icon
 // @match        https://somesite.com/*
 // @grant        none
+// @updateURL    https://github.com/Qwox0/.dotfiles/raw/main/tampermonkey/template.user.js
+// @downloadURL  https://github.com/Qwox0/.dotfiles/raw/main/tampermonkey/template.user.js
 // @sandbox      JavaScript
 // ==/UserScript==
 
