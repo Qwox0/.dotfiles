9904cd80f46dfd3cf53d5582a8d6a0920a5d5bd2
Qwox <qwox@qwox.com>
2023-12-10 14:18:15 +0100

    tampermonkey: yt_hide_gradient: log STATE at the end

--- tampermonkey/yt_hide_gradient.user.js
+++ tampermonkey/yt_hide_gradient.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         yt player hide gradient
 // @namespace    qwox
-// @version      0.3.2
+// @version      0.3.3
 // @description  hides annoying gradient shown on the bottom half of the youtube video player when the user agent is modified in a specific way.
 // @author       Qwox
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -49,6 +49,7 @@ function main() {
             error(e);
         } finally {
             log("finished running main");
+            log("STATE:", STATE);
         }
     }
 
