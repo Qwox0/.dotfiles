63b334b0facf3e6c1d90188da747ec40cc030110
Qwox <qwox@qwox.com>
2024-02-02 14:22:11 +0100

    nvim: treesitter context max height

--- nvim/after/plugin/treesitter/context.lua
+++ nvim/after/plugin/treesitter/context.lua
@@ -1,16 +1,21 @@
 if not require("qwox.util").has_plugins("treesitter-context") then return end
 
 function ContextSetup(show_all_context)
-    require("treesitter-context").setup({
-        enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
-        throttle = true, -- Throttles plugin updates (may improve performance)
-        max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+    require("treesitter-context").setup {
+        enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true,         -- Throttles plugin updates (may improve performance)
+        max_lines = 8,           -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0,   -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 5, -- Maximum number of lines to show for a single context
+        trim_scope = "outer",    -- Which context lines to discard if `max_lines` is exceeded. Choices: "inner", "outer"
+        mode = "cursor",         -- Line used to calculate context. Choices: "cursor", "topline"
         show_all_context = show_all_context,
         patterns = {
             -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
             -- For all filetypes
             -- Note that setting an entry here replaces all other patterns for this entry.
-            -- By setting the 'default' entry below, you can control which nodes you want to
+            -- By setting the "default" entry below, you can control which nodes you want to
             -- appear in the context window.
             default = {
                 "function",
@@ -31,7 +36,7 @@ function ContextSetup(show_all_context)
                 "else_clause",
             },
         },
-    })
+    }
 end
 
 --vim.keymap.set("n", "<leader>cf", function() ContextSetup(true) end)
