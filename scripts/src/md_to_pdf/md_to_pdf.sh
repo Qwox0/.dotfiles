49ab4254772ebf6573ca14722d7a71fab8d59979
Qwox <qwox@qwox.com>
2024-04-27 15:24:02 +0200

    md_to_pdf: add github flavored markdown style

--- scripts/src/md_to_pdf/md_to_pdf.sh
+++ scripts/src/md_to_pdf/md_to_pdf.sh
@@ -1,7 +1,58 @@
 #!/usr/bin/env bash
 
 script="$(readlink -f $0)"
-script_path="$(dirname $script)"
+script_dir="$(dirname $script)"
+
+# ---------------------------------
+
+print_usage() {
+    echo "USAGE: md_to_pdf [OPTIONS] [MD FILE]"
+    echo "  -h, -?, --help              Show this help message"
+    echo ""
+    echo "  --style=github [default]    Use GitHub Flavored Markdown Spec"
+    echo "  --style=pandoc              Use Default Pandoc Styling"
+}
+
+github_style="--standalone --css $script_dir/github_no_body_class.css"
+pandoc_style=""
+style="$github_style"
+
+for i in "$@"; do
+    case $i in
+        -h|-?|--help)
+            print_usage
+            exit 0
+            ;;
+        # --style=*)
+        #     style="${i#*=}"
+        #     shift
+        #     ;;
+        --style=github)
+            style="$github_style"
+            shift
+            ;;
+        --style=pandoc)
+            style="$pandoc_style"
+            shift
+            ;;
+        -*|--*)
+            echo "Unknown option $i"
+            print_usage
+            exit 1
+            ;;
+        *)
+            ;;
+    esac
+done
+
+in_file="$1"
+
+if [ -z "$in_file" ]; then
+    echo "please provide a markdown file" >&2
+    exit 2
+fi
+
+# ---------------------------------
 
 compiler="pandoc"
 install_cmd="sudo apt install pandoc wkhtmltopdf"
@@ -13,30 +64,15 @@ if ! type "$compiler" > /dev/null; then
     exit 1
 fi
 
-in_file="$1"
-
-if [ -z "$in_file" ]; then
-    echo "please provide a markdown file" >&2
-    exit 2
-fi
-
 name="${in_file%.*}"
-
-css_file="$script_path/github.css"
-
-# $compiler -f gfm -t html5 \
-#     --metadata pagetitle="${in_file##*/}" \
-#     --pdf-engine-opt=--enable-local-file-access \
-#     --css $css_file \
-#     $in_file \
-#     -o "$name.pdf"
 
 for ext in "html" "pdf"; do
     echo "Generate $ext"
     pandoc -f markdown+pipe_tables -t html5 \
         --metadata pagetitle="${in_file##*/}" \
         --mathml \
+        $style \
         --pdf-engine-opt=--enable-local-file-access \
         "$in_file" \
         -o "$name.$ext"
-done
+    done
