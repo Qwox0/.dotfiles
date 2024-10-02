a3992acc9832a58203a708bb4fa407f3f0eb0f05
Qwox <qwox@qwox.com>
2024-10-02 11:28:59 +0200

    ansible: fix installation script, improve `run.sh`

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
+info() {
+    echo -e "\e[92;1m+++\e[0m $1"
+}
+
+warn() {
+    echo -e "\e[93;1m+++ WARN:\e[0m $1"
+}
+
+error() {
+    echo -e "\e[91;1m+++ ERROR:\e[0m $1" >&2
+    exit $2
+}
+
 ansible_path=${0%/*}
 
 local_yml="$ansible_path/local.yml"
+
+ansible_playbook_cmd="ansible-playbook"
+
+if [ ! $(which "$ansible_playbook_cmd") ]; then
+    warn "Couldn't find \`ansible-playbook\` in PATH. using default"
+    ansible_playbook_cmd="~/.local/bin/ansible-playbook"
+fi
+
+__usage="
+Usage: $(basename $0) [OPTIONS] tag
+
+Options:
+  -K, -p, -r, -s            ask for BECOME password (default)
+  -n, -u                    try to run script without BECOME password
+"
 
 # BECOME password
 ask_pw=true
@@ -14,37 +42,34 @@ while getopts "Kprsnu" OPTION; do
         s) ask_pw=true ;;
         n) ask_pw=false ;;
         u) ask_pw=false ;;
-        ?)
-            echo "script usage: $(basename $0) [opt] tag" >&2
-            echo "[opt]:"
-            echo "  -K, -p, -r, -s: ask for BECOME password (default)"
-            echo "  -n, -u        : try to run script without BECOME password"
-            exit 1
-            ;;
+        ?) error "$__usage" 1 ;;
     esac
 done
 shift "$(($OPTIND -1))"
 
-tags_str="$(ansible-playbook local.yml --list-tags | grep "TASK TAGS:" | sed "s/^\s*TASK TAGS: \[\(.*\)\]$/\1/")"
+tags_str="$($ansible_playbook_cmd $local_yml --list-tags | grep "TASK TAGS:" | sed "s/^\s*TASK TAGS: \[\(.*\)\]$/\1/")"
 IFS=', ' read -r -a tags <<< "$tags_str"
 
 tag="$1"
 
 IFS=$'\n'
 if [ -z "$(echo "${tags[*]}" | grep "^$tag$")" ]; then
-    echo "please provide tag!"
-    echo "the following tags are available:"
-    printf '    * `%s`\n' "${tags[@]}"
-    echo -e "\nIf you want to run all tasks use \`ansible-playbook $local_yml\`"
-    exit 2
+    tag_list_fancy="$(printf '    * `%s`\n' "${tags[@]}")"
+    err="please provide tag!
+the following tags are available:
+${tag_list_fancy}
+
+If you want to run all tasks use \`ansible-playbook $local_yml\`
+"
+    error "$err" 2
 fi
 unset IFS
 
 if ! $ask_pw; then
-    ansible-playbook $local_yml -t $tag
+    $ansible_playbook_cmd $local_yml -t $tag
 
     if [ $? == 0 ]; then
-        echo "finished running ansible playbook. :)"
+        info "finished running ansible playbook. :)"
         exit 0
     fi
 
@@ -55,12 +80,10 @@ if ! $ask_pw; then
     if [[ $input != "" && $input != "y" && $input != "Y" ]]; then exit 3; fi
 fi
 
-ansible-playbook $local_yml -K -t $tag
+$ansible_playbook_cmd $local_yml -K -t $tag
 
 if [ $? == 0 ]; then
-    echo "finished running ansible playbook. :)"
-    exit 0
+    info "finished running ansible playbook. :)"
 else
-    echo "failed to run ansible playbook. :("
-    exit 3
+    error "failed to run ansible playbook. :(" 3
 fi
